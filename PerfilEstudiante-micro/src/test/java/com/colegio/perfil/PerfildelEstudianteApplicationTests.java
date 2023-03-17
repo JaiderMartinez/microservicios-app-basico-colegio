8bf49828df29ecacbbf829b03ae27b01a46091f1
JaiderSMS18 <smithsaemirto45@gmail.com>
2023-03-16 19:53:23 -0500

    cambio en el nombre de las carpetas test

--- PerfilEstudiante-micro/src/test/java/com/colegio/perfil/PerfildelEstudianteApplicationTests.java
+++ PerfilEstudiante-micro/src/test/java/com/colegio/perfil/PerfildelEstudianteApplicationTests.java
@@ -1,4 +1,4 @@
-package com.colegio.materias;
+package com.colegio.perfil;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- Usuario-micro/src/test/java/com/colegio/UsuarioApplicationTests.java
+++ Usuario-micro/src/test/java/com/colegio/UsuarioApplicationTests.java
@@ -1,4 +1,4 @@
-package com.martinez;
+package com.colegio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
