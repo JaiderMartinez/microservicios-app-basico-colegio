7425703b51f1c8e41528d0051909839b90a6f0d4
JaiderSMS18 <smithsaemirto45@gmail.com>
2023-03-26 16:15:49 -0500

    mock de integracion

--- PerfilEstudiante-micro/src/main/java/com/colegio/perfil/controllers/EstudianteController.java
+++ PerfilEstudiante-micro/src/main/java/com/colegio/perfil/controllers/EstudianteController.java
@@ -1,13 +1,11 @@
 package com.colegio.perfil.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +31,8 @@ public class EstudianteController {
 	@PostMapping("Perfil")
 	public ResponseEntity<Estudiante> guardarEstudiante(@RequestBody Estudiante estudiante) {
 		Estudiante nuevoEstudiante = estudianteService.save(estudiante);
-		return ResponseEntity.ok(nuevoEstudiante);
+		return new ResponseEntity<Estudiante>(nuevoEstudiante, HttpStatus.CREATED);
+
 	}
 	
 }
